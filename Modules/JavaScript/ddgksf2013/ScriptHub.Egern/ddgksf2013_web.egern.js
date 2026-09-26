195d5628cda466383b842b4cee226328016d1b3d
ifflagged <91680218+ifflagged@users.noreply.github.com>
2026-09-26 22:50:09 +0800

    Delete Modules directory

--- Modules/JavaScript/ddgksf2013/ScriptHub.Egern/ddgksf2013_web.egern.js
+++
@@ -1,2 +0,0 @@
-body = $response.body.replace(/Lock\s*=\s*\d/g, 'Lock=5').replace(/<\/i>\s*QuantumultX/g, '</i> Egern');
-$done({ body });
--- Modules/JavaScript/mist-whisper/WeChat_Enhance/resource.wuyra.com_LAWSON.js
+++
@@ -1,9 +0,0 @@
-let obj;
-try {
-  obj = JSON.parse($response.body);
-  obj.data?.homeButtonList && delete obj.data.homeButtonList;
-  obj.data?.dysmorphismPictureList && delete obj.data.dysmorphismPictureList;
-  $done({ body: JSON.stringify(obj) });
-} catch {
-  $done({});
-}
